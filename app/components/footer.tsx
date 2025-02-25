6d893f70fb14da8f3196d4d03805a22f3980eebd
Justin Ikenouye <jikenouye@radiclebalance.com>
2025-02-25 15:52:12 -0700

    Background image fixes

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,11 +1,13 @@
+import rocks from "../../public/rocks.jpg";
+
 export default function Footer() {
   return (
     <>
-      <div className="mask-image-reverse absolute z-[-1]">
+      <div className="mask-image-reverse absolute" style={{ backgroundImage: `url(${rocks.src})` }}>
         <div className="bg-[url(/rocks.jpg)] bg-cover bg-no-repeat bg-center w-screen h-[50rem]" />
       </div>
       <div className="w-screen h-[50rem] flex items-end justify-center pb-6">
-        <div className="text-1xl p-3 bg-black transition-shadow shadow-0 hover:shadow-5">Photos by me :)</div>
+        <div className="text-1xl p-3 bg-black transition-shadow shadow-0 hover:shadow-5 z-[1]">Photos by me :)</div>
       </div>
     </>
   )
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,13 +4,13 @@ import waves from "../../public/waves.jpg";
 export default function Header() {
   return (
     <>
-      <div className="mask-image absolute z-[-1]">
-          <div className=' bg-cover bg-no-repeat bg-center w-screen h-[50rem] flex items-center justify-center' 
+      <div className="mask-image absolute">
+          <div className='bg-cover bg-no-repeat bg-center w-screen h-[50rem] flex items-center justify-center' 
           style={{ backgroundImage: `url(${waves.src})`}} />
         </div>
         <div className="w-screen p-6 flex items-center justify-between gap-6">
-          <div className="text-2xl p-2 bg-black transition-shadow shadow-0 hover:shadow-5">Justin Ikenouye</div>
-          <div className="flex gap-3">
+          <div className="text-2xl p-2 bg-black transition-shadow shadow-0 hover:shadow-5 z-[1]">Justin Ikenouye</div>
+          <div className="flex gap-3 z-[1]">
             <a
               href="https://github.com/JustinIke"
               target="_blank"
@@ -39,16 +39,16 @@ export default function Header() {
           </div>
         </div>
         <div className="w-screen h-80 flex items-center justify-center">
-          <div className="text-4xl mx-6 p-3 bg-black transition-shadow shadow-0 hover:shadow-5 text-center">Hey, I'm Justin - Software Developer</div>
+          <div className="text-4xl mx-6 p-3 bg-black transition-shadow shadow-0 hover:shadow-5 text-center z-[1]">Hey, I'm Justin - Software Developer</div>
         </div>
 
         <div className="flex flex-col md:flex-row justify-center mx-6 gap-3">
-          <div className="md:w-3/5 lg:w-1/3">
+          <div className="md:w-3/5 lg:w-1/3 z-[1]">
             <div className="text-2xl p-3 bg-black transition-shadow shadow-0 hover:shadow-5">
               I'm passionate about creating smooth user experiences alongside compelling interfaces.
             </div>
           </div>
-          <div className="md:w-2/5 lg:w-1/5">
+          <div className="md:w-2/5 lg:w-1/5 z-[1]">
             <div className="text-2xl p-3 bg-black transition-shadow shadow-0 hover:shadow-5 flex flex-col gap-3">
               Core Skills
               <div className="flex items-center gap-3">
